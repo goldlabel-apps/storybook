d7bbcef6bc89b82f0c7822281665ff3888f18422
Chris <listingslab@gmail.com>
2019-07-22 07:21:47 +1000

    feat(tools): deploy to firebase

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const color = require('gulp-color');
 
 function copyBuild(cb) {
   console.log (color('gulp copy storybook-static', 'GREEN'));
-  return src('build/**/*')
+  return src('storybook-static/**/*')
           .pipe(dest('firebase/storybook'))
 }
 exports.copyBuild = copyBuild;
