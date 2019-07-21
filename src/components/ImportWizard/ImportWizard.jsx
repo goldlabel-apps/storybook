554bd940e553ee4c52c111eeb0a02c74937e842f
Chris <listingslab@gmail.com>
2019-07-21 15:10:10 +1000

    feat(ImportWizard)

--- src/components/ImportWizard/ImportWizard.jsx
+++ src/components/ImportWizard/ImportWizard.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
-function ImportWizard() {
+const useStyles = makeStyles(theme => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+  input: {
+    display: 'none',
+  },
+}));
+
+export default function ImportWizard() {
+  const classes = useStyles();
   return (
-    <div className="HMLR">
-      ImportWizard
-      composition 1
+    <div>
+      <Button
+        variant={`contained`} 
+        color={`primary`} 
+        className={classes.button}>
+        Open Import Wizard
+      </Button>
+      
     </div>
   );
 }
-
-export default ImportWizard;
