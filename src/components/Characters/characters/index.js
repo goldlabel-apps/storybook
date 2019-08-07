7fe82295d03db537cd6dc1f2c9e65d4c0a2bd973
Chris <listingslab@gmail.com>
2019-08-07 20:58:01 +1000

    .

--- src/components/Characters/characters/index.js
+++ src/components/Characters/characters/index.js
@@ -3,6 +3,11 @@ import Chix from './Chix';
 import Dapper from './Dapper';
 import Hippy from './Hippy';
 import Hipster from './Hipster';
+import Mumma from './Mumma';
+import Punk from './Punk';
+import Rasta from './Rasta';
+import Rocker from './Rocker';
+import Youngun from './Youngun';
 
 export {
     Biker,
@@ -10,4 +15,9 @@ export {
     Dapper,
     Hippy,
     Hipster,
+    Mumma,
+    Punk,
+    Rasta,
+    Rocker,
+    Youngun,
 };
