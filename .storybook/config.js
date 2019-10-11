ca5990d8ba0803029d0bf4f23a50345bba04b4d8
noobie2019 <noobie2019@listingslab.com>
2019-10-11 10:02:34 +1000

    No develop branch

---
+++ .storybook/config.js
@@ -0,0 +1,5 @@
+import { configure } from '@storybook/react';
+function loadStories() {
+  require('../src/stories');
+}
+configure(loadStories, module);
---
+++ docs/index.md
@@ -0,0 +1,4 @@
+# Storybook
+
+[What is Storybook?](./docs/what-is-storybook.md) 
+It's The construct. Like in the Matrix.
