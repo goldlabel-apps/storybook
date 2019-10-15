63d1780a310073d4620c126c3671985f4dddfb01
listingslab <listingslab@gmail.com>
2019-10-15 21:02:02 +1000

    Focus on storybook only

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM docker.cwx.io/codeworx/node:11
-COPY . /app
-WORKDIR /app
-ENTRYPOINT ["/app/node_modules/.bin/sparse"]
--- functions/index.js
+++
@@ -1,5 +0,0 @@
-const functions = require('firebase-functions');
-
-exports.storybook = functions.https.onRequest((request, response) => {
- response.send("Hello from Storybook");
-});
